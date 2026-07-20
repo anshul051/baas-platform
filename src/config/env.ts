6f01fdf1bc127fb6be2996dc08ab6eddc86e89bb
anshul051 <anshulkumararya51@gmail.com>
2026-07-21 02:08:16 +0530

    feat: make logger environment-aware, disable exactOptionalPropertyTypes

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,6 +4,7 @@ import { z } from 'zod';
 
 const envSchema = z.object({
     PORT: z.coerce.number(),
+    NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
 });
 
 const env = envSchema.parse(process.env);
--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,14 +1,17 @@
-import pino from 'pino';
-import env from './env.js';
+import pino from "pino";
+import env from "./env.js";
 
 const logger = pino({
-    level: 'info',
-    transport: {
-        target: 'pino-pretty',
-        options: {
+  level: "info",
+  transport:
+    env.NODE_ENV === "development"
+      ? {
+          target: "pino-pretty",
+          options: {
             colorize: true,
-        },
-    },
+          },
+        }
+      : undefined, // if NODE_ENV is not development, don't use pino-pretty
 });
 
 export default logger;
